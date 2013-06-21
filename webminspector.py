e34cc7d33482d6ec062c9e7ccf131277bef6eca8
John Koleszar <jkoleszar@google.com>
2013-06-20 18:08:13 -0700

    Update webminspector to include LICENSE
    Same terms as apply to libvpx.
    
    Change-Id: I6414a4994a91493edbef58276df07c0ba83b852d

--- webminspector.py
+++ webminspector.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python2.4
-#
-# Copyright 2010 Google Inc. All Rights Reserved.
+##
+##  Copyright (c) 2010 The WebM project authors. All Rights Reserved.
+##
+##  Use of this source code is governed by a BSD-style license
+##  that can be found in the LICENSE file in the root of the source
+##  tree. An additional intellectual property rights grant can be found
+##  in the file PATENTS.  All contributing project authors may
+##  be found in the AUTHORS file in the root of the source tree.
+##
 
 """One-line documentation for webmtool module.
 
